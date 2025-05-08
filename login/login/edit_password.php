fb4564a1fad3452517d045ea036f71c8c1310e8f
lee010l <165228440+lee010l@users.noreply.github.com>
2025-05-09 00:02:34 +0800

    Update edit_password.php

--- login/login/edit_password.php
+++ login/login/edit_password.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $servername = "localhost";
         $username = "root";
         $password = "";
-        $dbname = "users_db";
+        $dbname = "burger4.0";
 
         $conn = new mysqli($servername, $username, $password, $dbname);
         if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
